0f0ed5f8ca98fb7436b4e7265a82cfc62046b5c4
NEYAZUDDIN5050 <132392596+NEYAZUDDIN5050@users.noreply.github.com>
2024-12-08 11:13:26 +0530

    Login

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
     <>
-      <div className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400">
-        eihjbgsedfjhvvb
-      </div>
+      <Login />
     </>
   );
 }
